d91eef6f146f98bd1cac26ddde62c3639f9cbd11
Joo X Nano <106291265+FLAME-XD@users.noreply.github.com>
2023-05-02 22:05:36 +0700

    Update exorcist.py

--- exorcist.py
+++ exorcist.py
@@ -11,7 +11,7 @@ console = Console()
 # ---> MainStart
 if __name__=="__main__":
 #  try:os.system("main/python .ex.py")
-  try:os.system("python .run.py")
+  try:os.system("python .ex.py")
   except requests.exceptions.ConnectionError as e:
       Console(width=70).print(Panel(f'''{str(e).title()}''',style=f"bold white",padding=(0,4)))
       sys.exit()
